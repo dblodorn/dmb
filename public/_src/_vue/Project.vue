36c1e0da977a744bac33a9c1bbc24c5bfd48e58b
DAIN BLODORN <dain.blodo@gmail.com>
2016-11-01 21:54:00 -0700

    responsive and jquery

--- public/_src/_vue/Project.vue
+++ public/_src/_vue/Project.vue
@@ -1,17 +1,17 @@
 <template lang="jade">
   h1 {{project.project_name}}
-  aside(v-if="$mq.resize && $mq.below(width, 800)" v-bind:style="{ backgroundImage: 'url(' + imgpath + $route.params.slug + '/' + $route.params.slug + '@0.5x.jpg)' }" transition="home")
-  aside(v-if="$mq.resize && $mq.between(width, [800,1919])" v-bind:style="{ backgroundImage: 'url(' + imgpath + $route.params.slug + '/' + $route.params.slug + '.jpg)' }" transition="home")
-  aside(v-if="$mq.resize && $mq.above(width, 1920)" v-bind:style="{ backgroundImage: 'url(' + imgpath + $route.params.slug + '/' + $route.params.slug + '@2x.jpg)' }" transition="home")
-  section.single-project
+  aside(v-bind:class="{asidescroll: project.images}" v-if="$mq.resize && $mq.below(width, 800)" v-bind:style="{ backgroundImage: 'url(' + imgpath + $route.params.slug + '/' + $route.params.slug + '@0.5x.jpg)' }" transition="home")
+  aside(v-bind:class="{asidescroll: project.images}" v-if="$mq.resize && $mq.between(width, [800,1919])" v-bind:style="{ backgroundImage: 'url(' + imgpath + $route.params.slug + '/' + $route.params.slug + '.jpg)' }" transition="home")
+  aside(v-bind:class="{asidescroll: project.images}" v-if="$mq.resize && $mq.above(width, 1920)" v-bind:style="{ backgroundImage: 'url(' + imgpath + $route.params.slug + '/' + $route.params.slug + '@2x.jpg)' }" transition="home")
+  section.single-project(v-bind:class="{sectionscroll: project.images}")
     article.project-description
       p.project-copy {{{project.project_copy}}}
       .bottom-buttons
         a.btn(v-for="link in project.links" v-link="link.site_link" target="_blank") {{link.link_description}}
         a.btn(v-link="'/work'") ALL WORK
-  ul.images(v-if="project.images")
-    li(v-for="image in project.images")
-      img(v-bind:src="imgpath + '/' + $route.params.slug + '/' + image.project_image" v-bind:alt="image.image_alt")
+    ul.images(v-if="project.images")
+      li(v-for="image in project.images")
+        img(v-bind:src="imgpath + '/' + $route.params.slug + '/' + image.project_image" v-bind:alt="image.image_alt")
 </template>
 
 <script>
@@ -33,11 +33,8 @@
     route: {
       canReuse: false,
       activate: function() {
-        
         utility.setId('body','project')
-
         $('body').animate({ scrollTop: 0 }, 5);
-        
         $('#dbk').addClass('fade-out');
         
         var id = this.$route.params.slug
@@ -47,7 +44,16 @@
         },
         function (data) {
           alert('Failed to load data');
-        }); 
+        });
+        /*
+        setTimeout(function(){
+          var wh = window.innerHeight;
+          if($('.single-project').hasClass('sectionscroll')){
+            var descriptionHeight = $('.project-description').innerHeight() + 10;
+            $('section.single-project').css('margin-top', 'calc(100vh - ' + descriptionHeight + 'px)');
+          }
+        }, 50);
+        */
       }
     }
   }
@@ -87,24 +93,39 @@
       position: fixed
       width: 100vw
       height: 100vh
+    h1
+      top: 0
+      left: 0
     section.single-project
       position: fixed
       height: 100vh
-      align-items: flex-end
       padding: 3rem
-      h1
-        top: 0
-        left: 0
       article.project-description
         @extend %black-shadow
         max-width: 60rem
         margin-left: auto
         background-color: $white
         border: 2px solid $black
-      ul.images
-        padding: 0 2rem 4rem
       .bottom-buttons
         flex-direction: row
+    
+    .asidescroll
+      //position: relative!important
+      height: calc(100vh - 20rem)!important
+    
+    .sectionscroll
+      position: relative!important
+      height: initial!important
+      padding: .5rem!important
+      flex-wrap: wrap
+      border-top: 2px solid $black
+      margin-top: calc(100vh - 22rem)
+      background-color: $white
+      article
+        align-self: flex-start!important
+        width: 100vw!important
+        border: 0!important
+        box-shadow: none!important
 
   aside
     @extend %full-bg
@@ -136,11 +157,11 @@
       line-height: 1.125
 
   section.single-project
-    top: 0
+    display: flex
     z-index: $content-z
     width: 100vw
-    display: flex
-    article.project-description
+    article
+      align-self: flex-end
       padding: 2rem
     a.project-link
       display: block
@@ -153,17 +174,18 @@
         color: $black 
 
   ul.images
-    @extend %aligner
-    @extend %flex-row-wrap
-    position: absolute
-    max-width: 50rem
+    display: flex
+    position: relative
+    width: 100vw
     background-color: $white
-    padding: 1rem 2rem
-    border: 2px solid $black
+    padding: 0
     z-index: 9000
-    margin: 10rem 3rem
+    flex-wrap: wrap
     li
-      align-self: center
-      padding: 1rem 0
+      padding: .5rem
+      width: 50%
+      align-self: flex-end
+    li:nth-child(4n)
+      align-self: flex-start 
 
 </style>
